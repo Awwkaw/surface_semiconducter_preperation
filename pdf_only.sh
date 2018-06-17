ff944a6c14cdaf8ddefc92c7a59f783627c3683c
awwkaw <awwkaw@gmail.com>
2018-06-17 10:52:53 +0200

    updated the pdf_only script such that it now doesnt ask to owerwrite files

--- pdf_only.sh
+++ pdf_only.sh
@@ -1,2 +1,2 @@
-for subdir in *; do cp "$subdir"/"$subdir""_short.pdf" "pdf_files"/; done;
-for subdir in *; do cp "$subdir"/"$subdir"".pdf" "pdf_files"/; done;
+for subdir in *; do yes | cp -rf "$subdir"/"$subdir""_short.pdf" "pdf_files"/; done;
+for subdir in *; do yes | cp -rf "$subdir"/"$subdir"".pdf" "pdf_files"/; done;
